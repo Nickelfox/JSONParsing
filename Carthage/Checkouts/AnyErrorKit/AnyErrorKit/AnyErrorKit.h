f7c6774e349f589c834ba42e97f24f2e7e565004
Vaibhav Parmar <vaibhav@nickelfox.com>
2018-01-08 20:35:34 +0530

    refactor: carthage support

---
+++ Carthage/Checkouts/AnyErrorKit/AnyErrorKit/AnyErrorKit.h
@@ -0,0 +1,19 @@
+//
+//  AnyErrorKit.h
+//  AnyErrorKit
+//
+//  Created by Vaibhav Parmar on 08/01/18.
+//  Copyright © 2018 Nickelfox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnyErrorKit.
+FOUNDATION_EXPORT double AnyErrorKitVersionNumber;
+
+//! Project version string for AnyErrorKit.
+FOUNDATION_EXPORT const unsigned char AnyErrorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnyErrorKit/PublicHeader.h>
+
+
