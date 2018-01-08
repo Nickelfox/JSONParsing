d5d0b6e4f4b58363b30d81a9fc71cbda07978a1a
Vaibhav Parmar <vaibhav@nickelfox.com>
2018-01-08 20:15:55 +0530

    add: carthage support

---
+++ JSONParsing/JSONParsing.h
@@ -0,0 +1,19 @@
+//
+//  JSONParsing.h
+//  JSONParsing
+//
+//  Created by Vaibhav Parmar on 08/01/18.
+//  Copyright © 2018 Nickelfox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONParsing.
+FOUNDATION_EXPORT double JSONParsingVersionNumber;
+
+//! Project version string for JSONParsing.
+FOUNDATION_EXPORT const unsigned char JSONParsingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONParsing/PublicHeader.h>
+
+
