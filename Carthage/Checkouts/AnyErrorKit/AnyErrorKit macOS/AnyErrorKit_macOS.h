8aae7afc56e78901a22bb717dff2b38e9f9f2cb3
Vaibhav Parmar <vaibhav@nickelfox.com>
2018-01-09 20:52:19 +0530

    add: framework for macOS, tvOS

---
+++ Carthage/Checkouts/AnyErrorKit/AnyErrorKit macOS/AnyErrorKit_macOS.h
@@ -0,0 +1,19 @@
+//
+//  AnyErrorKit_macOS.h
+//  AnyErrorKit macOS
+//
+//  Created by Vaibhav Parmar on 09/01/18.
+//  Copyright © 2018 Nickelfox. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AnyErrorKit_macOS.
+FOUNDATION_EXPORT double AnyErrorKit_macOSVersionNumber;
+
+//! Project version string for AnyErrorKit_macOS.
+FOUNDATION_EXPORT const unsigned char AnyErrorKit_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnyErrorKit_macOS/PublicHeader.h>
+
+
